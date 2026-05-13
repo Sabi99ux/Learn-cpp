f6fa27cef98216b589daf7beb5047433030cf222
Sabi99ux <sabibachtiar@gmail.com>
2026-05-13 17:09:32 +0700

    feat: Menambahkan UUID generator utility

---
+++ CliDB/src/uuid_generator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <iomanip>
+
+std::string uuid_generator() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 15);
+
+    std::stringstream ss;
+
+    for (int i = 0; i < 32; i++) {
+        ss << std::hex << dis(gen);
+        if (i == 7 || i == 11 || i == 15 || i == 19)
+            ss << "-";
+    }
+
+    return ss.str();
+}
